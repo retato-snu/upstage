e46f571021bccb9b8b8a10b06b1e102e39928279
Joongwon Ahn <freleefty@gmail.com>
2026-03-30 16:45:31 +0900

    Add regenerate and stop button

--- components/chat/chat-assistant.tsx
+++ components/chat/chat-assistant.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { BotIcon, CornerDownLeftIcon, UserIcon } from "lucide-react";
+import { BotIcon, CornerDownLeftIcon, UserIcon, RefreshCcwIcon, SquareIcon } from "lucide-react";
 import { memo, useEffect, useRef, useState } from "react";
 import { DefaultChatTransport } from "ai";
 import { useChat } from "@ai-sdk/react";
@@ -15,31 +15,52 @@ import {
 import type { ChatMessage } from "@/app/api/chat/route";
 
 export default function ChatAssistant() {
-  const { messages, sendMessage, status } = useChat<ChatMessage>({
+  const { messages, sendMessage, regenerate, stop, status } = useChat<ChatMessage>({
     transport: new DefaultChatTransport({
       api: "/api/chat",
     }),
+    experimental_throttle: 50,
   });
   const [input, setInput] = useState("");
   const inputRef = useRef<HTMLInputElement>(null);
-  const bottomRef = useRef<HTMLDivElement>(null);
 
+  const bottomRef = useRef<HTMLDivElement>(null);
+  const lastScrollTop = useRef(0);
+  const autoScroll = useRef(true);
   useEffect(() => {
-    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+    if (autoScroll.current)
+      bottomRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setInput("");
+    autoScroll.current = true;
     await sendMessage({
       parts: [{ type: "text", text: input }],
     });
     inputRef.current?.focus();
+    autoScroll.current = false;
   };
+
+  const handleRetry = async () => {
+    autoScroll.current = true;
+    await regenerate();
+    autoScroll.current = false;
+  }
+
+  const handleStop = async () => {
+    await stop();
+    autoScroll.current = false;
+  }
 
   return (
     <div className="flex h-full max-h-[60vh] flex-col gap-4">
-      <div className="overflow-y-auto pr-2">
+      <div className="overflow-y-auto pr-2" onScroll={(e) => {
+        if (e.currentTarget.scrollTop < lastScrollTop.current)
+          autoScroll.current = false;
+        lastScrollTop.current = e.currentTarget.scrollTop;
+      }}>
         {messages.map((message) => (
           <div key={message.id} className="flex items-start">
             <div className="flex-0">
@@ -88,16 +109,37 @@ export default function ChatAssistant() {
             placeholder="메시지를 입력하세요..."
             ref={inputRef}
           />
-          <InputGroupAddon align="block-end">
-            <InputGroupButton
-              type="submit"
-              disabled={status !== "ready" || input.trim() === ""}
-              size="icon-sm"
-              className="ml-auto"
-              variant="default"
-            >
-              <CornerDownLeftIcon className="size-4" />
-            </InputGroupButton>
+          <InputGroupAddon align="block-end" className="justify-end">
+            {messages.length > 0 ? (
+              <InputGroupButton
+                type="button"
+                disabled={status === "submitted" || status === "streaming"}
+                size="icon-sm"
+                variant="default"
+                onClick={handleRetry}
+              >
+                <RefreshCcwIcon className="size-4" />
+              </InputGroupButton>
+            ) : null}
+            {status === "ready" ? (
+              <InputGroupButton
+                type="submit"
+                disabled={input.trim() === ""}
+                size="icon-sm"
+                variant="default"
+              >
+                <CornerDownLeftIcon className="size-4" />
+              </InputGroupButton>
+            ) : (
+              <InputGroupButton
+                type="button"
+                size="icon-sm"
+                variant="default"
+                onClick={handleStop}
+              >
+                <SquareIcon className="size-4" />
+              </InputGroupButton>
+            )}
           </InputGroupAddon>
         </InputGroup>
       </form>
